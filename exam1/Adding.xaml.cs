d19545bf85beb90aba4951863d994d0bb27dc40b
Your Name <you@example.com>
2018-10-25 19:54:46 +0700

    init

---
+++ exam1/Adding.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace exam1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Adding : Page
+    {
+        public Adding()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            try
+            {
+                StorageFile readfFile = await folder.GetFileAsync(title.Text);
+                await FileIO.AppendTextAsync(readfFile, content.Text);
+
+            }
+            catch (Exception)
+            {
+                StorageFile file = await folder.CreateFileAsync(title.Text, CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteTextAsync(file, content.Text);
+            }
+            
+            
+        }
+    }
+}
---
+++ exam1/Detail.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace exam1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Detail : Page
+    {
+        public ObservableCollection<String> titleList = new ObservableCollection<String>();
+        public Detail()
+        {
+            
+            this.InitializeComponent();
+
+        }
+
+        private async void Detail_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            var readfFile = await folder.GetFilesAsync();
+            foreach (var titleFile in readfFile)
+            {
+                titleList.Add(titleFile.Name);
+            }
+            
+           
+
+        }
+
+        private async void showContent(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox bc = (ComboBox) sender;
+
+            Debug.WriteLine(bc.SelectedValue);
+            string fileName = bc.SelectedValue.ToString();
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var readfFile = await folder.GetFileAsync(fileName);
+            string a = await FileIO.ReadTextAsync(readfFile);
+            content.Text = a;
+
+            Debug.WriteLine(a);
+        }
+    }
+}
